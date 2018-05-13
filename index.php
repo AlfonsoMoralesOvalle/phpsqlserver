4378cef089aca7e1fa935fe983f362a1caf67eac
AlfonsoMoralesOvalle <denisadn@hotmail.com>
2018-05-13 12:23:50 -0600

    Iniciando Proyecto

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:dbserverseminario1.database.windows.net,1433; Database = dbseminario1", "seminario1", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "seminario1@dbserverseminario1", "pwd" => "{your_password_here}", "Database" => "dbseminario1", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:dbserverseminario1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+?>
+Error SQL SERVER V1.0 :-(
