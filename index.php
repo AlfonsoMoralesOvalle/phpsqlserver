d1c68c98bfa20d8dd3c62e83551866c1b179785f
AlfonsoMoralesOvalle <denisadn@hotmail.com>
2018-05-13 15:21:35 -0600

    ConexionSQLServer

--- index.php
+++ index.php
@@ -1,47 +1,39 @@
 <?php
-
- ReadData();
-
-    function OpenConnection()  
+    function Conexion()  
     {  
         try  
         {  
             $serverName = "tcp:dbserverseminario1.database.windows.net,1433";  
-            $connectionOptions = array("Database"=>"dbseminario1",  
-                "Uid"=>"seminario1", "PWD"=>"Seminario200925014");  
+            $connectionOptions = array("Database"=>"dbseminario1","UID"=>"seminario1", "PWD"=>"Seminario200925014");  
             $conn = sqlsrv_connect($serverName, $connectionOptions);  
-            if($conn == false)  
-                die(FormatErrors(sqlsrv_errors()));  
+			if( $conn === false ) {
+				die( print_r( sqlsrv_errors(), true));
+			}else{
+			 echo 'Conexion Exitosa';
+			    $sql = "SELECT id, nombre FROM comentarios";
+				$stmt = sqlsrv_query( $conn, $sql );
+				if( $stmt === false) {
+					die( print_r( sqlsrv_errors(), true) );
+				}else{
+
+				while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+					  echo "<br/>". $row['id'].", ".$row['nombre']."<br />";
+				}
+
+				sqlsrv_free_stmt( $stmt);
+				}
+			}				
+                
         }  
         catch(Exception $e)  
         {  
             echo("Error!");  
         }  
-    }
     
-function ReadData()  
-    {  
-        try  
-        {  
-            $conn = OpenConnection();  
-            $tsql = "SELECT nombre FROM comentarios";  
-            $getProducts = sqlsrv_query($conn, $tsql);  
-            if ($getProducts == FALSE)  
-                die(FormatErrors(sqlsrv_errors()));  
-            $productCount = 0;  
-            while($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC))  
-            {  
-                echo($row['nombre']);  
-                echo("<br/>");  
-                $productCount++;  
-            }  
-            sqlsrv_free_stmt($getProducts);  
-            sqlsrv_close($conn);  
-        }  
-        catch(Exception $e)  
-        {  
-            echo("Error!");  
-        }  
-    }       
+	
+	}
+	
+
+	Conexion();	    
 ?>
 
